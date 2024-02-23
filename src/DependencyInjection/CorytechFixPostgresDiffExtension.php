6b103b0997df467725b19eb6ce0a33bbf4957a14
Bohdan Pliachenko <bohdan.pliachenko@crosspay.net>
2024-02-23 13:53:53 +0200

    fix namespace

--- src/DependencyInjection/CorytechFixPostgresDiffExtension.php
+++ src/DependencyInjection/CorytechFixPostgresDiffExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Corytech\OpenApi\DependencyInjection;
+namespace Corytech\FixPostgresDiff\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
